85eccdd51b06433da9c35a14404fcea1c2cfaf90
Luana Leal <luana.leal.eq@gmail.com>
2020-08-26 02:00:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,13 @@ class Song
     new_song
   end
     
-  def self.new_by_name
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
     
+    def self.create_by_name(song_name)
+      
+    end 
 end
